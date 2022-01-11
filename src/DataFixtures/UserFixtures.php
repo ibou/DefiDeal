aaf4abb2dbf0a2ceeb628d0bf4ea2622b26b83f6
Ibrahima D <ibrahima.diallo@display-interactive.com>
2022-01-11 23:42:58 +0100

    add user+ fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+final class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; ++$i) {
+            $manager->persist($this->createUser($i));
+        }
+
+        $manager->flush();
+    }
+
+    private function createUser(int $index): User
+    {
+        $user = new User();
+        $user->setEmail(sprintf('user+%d@email.com', $index));
+        $user->setNickname(sprintf('user+%d', $index));
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+
+        return $user;
+    }
+}
